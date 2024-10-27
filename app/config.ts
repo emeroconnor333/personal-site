1c32c55a533a3a6820bc133b19b61fd07bd0c97b
emeroconnor333 <emeroconnor333@gmail.com>
2024-10-27 21:16:34 +0000

    Created personal site :)

--- app/config.ts
+++ app/config.ts
@@ -1,16 +1,16 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
-  ogImage: "/opengraph-image.png",
+  baseUrl: "https://personal-site-pi-swart-74.vercel.app/",
+  title: "Emer O'Connor",
+  name: "Emer O'Connor",
+  ogImage: "/opengraph-image.jpg",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "Hey, I'm Emer, a software engineering student. Here, you’ll find my projects and blog posts on what I’m building and what I'm learning along the way.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  twitter: "https://x.com/emeroconnor333",
+  github: "https://github.com/emeroconnor333",
+  instagram: "https://www.instagram.com/emeroconnor333",
+  linkedin: "https://www.linkedin.com/in/emer-o-connor-0b3387281/",
+  email: "mailto:emeroconnor333@gmail.com",
 };
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { socialLinks } from "./config";
 export default function Page() {
   return (
     <section>
-      <a href={socialLinks.twitter} target="_blank">
+      <a href={socialLinks.linkedin} target="_blank">
         <Image
           src="/profile.png"
           alt="Profile photo"
@@ -17,50 +17,26 @@ export default function Page() {
       </a>
 
       <h1 className="mb-8 text-2xl font-medium tracking-tight">
-        Portfolio, made simple!
+        My Personal Site
       </h1>
 
       <div className="prose prose-neutral dark:prose-invert">
         <p>
-          A clean, fast, and lightweight portfolio template built with Next.js,
-          Vercel, and Tailwind CSS for optimal performance.
+          Hey, I'm Emer, a software engineering student. Here, you’ll find my projects
+           and blog posts on what I’m building and what I'm learning along the way.
         </p>
         <p>
-          Nextfolio includes all the essentials for a stunning portfolio: SEO,
-          MDX support, RSS, Atom, & JSON feeds, analytics, tweet & YouTube
-          embeds, KaTeX integration, and{" "}
-          <a
-            target="_blank"
-            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
-          >
-            more
-          </a>
-          .
+          I post regularly about my experiences learning to code—the challenges, breakthroughs,
+           and everything in between. I also love sharing thoughts on the other things that
+           inspire me, from art and creativity to the books I’m reading and places I’m exploring.
         </p>
         <p>
-          Nextfolio is{" "}
-          <a href={socialLinks.github} target="_blank">
-            open-source
-          </a>{" "}
-          and fully customizable, making it easy to add more features.
+          In the projects section you'll see what I've completed, along with a glimpse into
+           what I’m currently working on.
         </p>
         <p>
-          <a
-            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
-            target="_blank"
-          >
-            Deploy
-          </a>{" "}
-          your Nextfolio site with Vercel in minutes and follow the set up
-          instructions in the{" "}
-          <a href="/blog/getting-started">Getting Started</a> post.
-        </p>
-        <p>
-          Built and maintained by{" "}
-          <a href="https://imsirius.xyz/" target="_blank">
-            Sirius
-          </a>
-          .
+          Check out my photo gallery for glimpses of the moments, places, and experiences that
+          inspire me beyond the screen.
         </p>
       </div>
     </section>
