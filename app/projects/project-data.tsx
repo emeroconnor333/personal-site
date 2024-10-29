4a52cc672ba0111c0ba34cee45b41cde8ea3929a
emeroconnor333 <emeroconnor333@gmail.com>
2024-10-29 18:32:59 +0000

    added EmpowerHer project.

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,24 +7,11 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Project One",
-    year: 2023,
+    title: "EmpowerHer Website",
+    year: 2024,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "I built this website to support my sister’s vision of launching wellness workshops for women. The site serves as a platform to promote events, share details, and connect with participants.",
+    url: "https://emeroconnor333.github.io/EmpowerHer/index.html",
   },
-  {
-    title: "Project Two",
-    year: 2022,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Three",
-    year: 2021,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
+
 ];
