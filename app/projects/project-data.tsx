bcae8b3e60d078b09af17f61bf20f39aefc69b4a
emeroconnor333 <emeroconnor333@gmail.com>
2024-11-10 17:13:25 +0000

    updates.

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -6,6 +6,12 @@ export interface Project {
 }
 
 export const projects: Project[] = [
+    {
+        title: "Plate Planner",
+        year: 2024,
+        description: "Plate Planner is a React-based meal planning web app that allows users to organize meals for each day of the week and automatically generates a shopping list based on selected recipes.",
+        url: "https://plate-planner-smoky.vercel.app/"
+    },
   {
     title: "EmpowerHer Website",
     year: 2024,
